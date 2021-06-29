2006da2465e8b24dc57b4f1a5dad7eac46df0534
peng <it_chenpeng@outlook.com>
2021-06-30 01:40:02 +0800

    add linkedList

--- DataStructures/LinearList/LinkList.cs
+++ DataStructures/LinearList/LinkList.cs
@@ -14,25 +14,51 @@ namespace LearnAlgorithm.DataStructures.LinearList
         private LinkListNode<T> _node;
         private int _length;
 
-        public T GetElement(int index)
+        public int Length => _length;
+
+        public LinkListNode<T> GetElement(int index)
         {
+            // 判断索引是否在当前范围内，不在范围内直接返回错误
             if (index < 0 || index > _length - 1)
                 throw new Exception("index 不在范围之内");
-
-            int j = 0;        
+            
             // 声明结点p
-            LinkListNode<T> p = _node.Next;
-            while (p != null && j < index)
+            LinkListNode<T> currentNode = _node.Next;
+            int currentCount = 0;
+            while (currentNode != null && currentCount < index)
             {
-                p = p.Next;
-                ++j;
+                currentNode = currentNode.Next;
+                ++currentCount;
             }
 
-            if (p == null || j > index)
+            if (currentNode == null || currentCount > index)
+                throw new Exception("元素不存在");
+            
+            return currentNode;
+        }
+
+        public void Insert(int index, T data)
+        {
+            var currentNode = GetElement(index);
+            if (currentNode == null)
                 throw new Exception("元素不存在");
 
-            T result = p.Data;
-            return result;
+            var newNode = new LinkListNode<T> {Data = data, Next = currentNode.Next};
+            currentNode.Next = newNode;
+
+            _length++;
+        }
+
+        public void Remove(int index)
+        {
+            var currentNode = GetElement(index);
+            if (currentNode == null)
+                throw new Exception("元素不存在");
+
+            var nextNode = currentNode.Next;
+
+            _length--;
+
         }
     }
 }
