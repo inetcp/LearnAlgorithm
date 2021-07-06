bbb494c9bc70bba48a0355658a143ddd324067b3
陈鹏 <it_chenpeng@outlook.com>
2021-07-06 23:31:48 +0800

    add fibonacci

---
+++ Algorithms/Fibonacci.cs
@@ -0,0 +1,43 @@
+﻿namespace LearnAlgorithm.Algorithms
+{
+    public class Fibonacci
+    {
+        /// <summary>
+        /// 斐波那契非递归版
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public int FibonacciNonRecursion(int n)
+        {
+            if (n < 3)
+                return n == 0 ? 0 : 1;
+            
+            int first = 1;
+            int second = 1;
+            int result = 0;
+            int i = 2;
+            while (i < n)
+            {
+                result = first + second;
+                first = second;
+                second = result;
+                i++;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// 斐波那契递归版
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public int FibonacciWithRecursion(int n)
+        {
+            if (n < 3)
+                return n == 0 ? 0 : 1;
+            
+            return FibonacciWithRecursion(n - 2) + FibonacciNonRecursion(n - 1);
+        }
+    }
+}
---
+++ UnitTest/AlgorithmsTests/FibonacciTest.cs
@@ -0,0 +1,23 @@
+﻿using LearnAlgorithm.Algorithms;
+using Xunit;
+
+namespace LearnAlgorithm.UnitTest.AlgorithmsTests
+{
+    public class FibonacciTest
+    {
+        [Fact]
+        public void Test()
+        {
+            Fibonacci f = new Fibonacci();
+            Assert.Equal(0, f.FibonacciNonRecursion(0));
+            Assert.Equal(1, f.FibonacciNonRecursion(1));
+            Assert.Equal(1, f.FibonacciNonRecursion(2));
+            Assert.Equal(13, f.FibonacciNonRecursion(7));
+            
+            Assert.Equal(0, f.FibonacciWithRecursion(0));
+            Assert.Equal(1, f.FibonacciWithRecursion(1));
+            Assert.Equal(1, f.FibonacciWithRecursion(2));
+            Assert.Equal(13, f.FibonacciWithRecursion(7));
+        }
+    }
+}
